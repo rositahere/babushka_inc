3c214c951d1bd8f976d0b7a3f2d79db5673efb41
Mai Hoang <maihh97@gmail.com>
2021-05-13 19:13:56 +0100

    Adding the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
+  resources :grannies, only: [:index, :show, :new, :create, :update]
+  resources :users, only: [:show] do
+    resources :appointments, only [:create]
+  end
+  resources :reviews, only: [:create]
 end
