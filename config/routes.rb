fed0baae1fb927499f35850a86c9196438299318
Emma Reitman <em_06@hotmail.co.uk>
2021-05-13 20:47:47 +0100

    updated routes, updated user schema, grannies view part completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
-  resources :grannies, only: [:index, :show, :new, :create, :update]
-  resources :users, only: [:show] do
+  resources :grannies, only: [:index, :show, :new, :create, :update] do
     resources :appointments, only: [:create]
   end
+  resources :users, only: [:show]
   resources :reviews, only: [:create]
 end
